35cf4e8e72c084f9eb43073e8f74310eb98f3c99
abeonweb <bmospecific@yahoo.com>
2023-11-14 21:11:12 +0100

    data file added

---
+++ data.js
@@ -0,0 +1,8 @@
+export const services = [
+    {id:0, href:"/services/standard-cleaning", title:"Standard Cleaning"},
+   { id:1, href:"/services/deep-cleaning", title:"Deep Cleaning"},
+   { id:2, href:"/services/disinfecting-services", title:"Disinfecting Services"},
+   { id:3, href:"/services/janitorial-services", title:"Janitorial Services"},
+   { id:4, href:"/services/carpet-cleaning", title:"Carpet Cleaning"},
+   {id:5, href:"/services/washroom-cleaning-and-consumables", title:"Washroom & Consumables" }
+];
