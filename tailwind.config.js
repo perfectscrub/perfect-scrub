b81f790734afff9d7f227da7b8dd032193437b52
abeonweb <bmospecific@yahoo.com>
2023-11-14 21:21:00 +0100

    tailwind config updated

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,36 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx,mdx}"
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'workSans': ["Work Sans"],
+        'plexSerif': ["IBM Plex Serif"]
+      },
+      spacing: {
+        18: "4.5rem",
+        22: "5.5rem",
+        30: "7.5rem",
+        89: "22.25rem",
+      },
+      borderWidth: {
+        3: "3px",
+        14: "14px",
+        18: "18px",
+      },
+      width: {
+        '97': '388px',
+        '98': '392px',
+        '99': '396px',
+        '100': '400px',
+        mc: "max-content",
+      },
+      height: {
+        '97': '388px',
+        '98': '392px',
+        '99': '396px',
+        '100': '400px'
+      },
+    },
   },
   plugins: [],
 }
