ef8d755e20a9ad9ea5de230e418ec0878d471989
abeonweb <bmospecific@yahoo.com>
2023-11-19 18:12:56 +0100

    added index facilities and testimonials data

--- data.js
+++ data.js
@@ -1,27 +1,79 @@
 export const services = [
-    {id:0, href:"/services/standard-cleaning", title:"Standard Cleaning"},
-   { id:1, href:"/services/deep-cleaning", title:"Deep Cleaning"},
-   { id:2, href:"/services/disinfecting-services", title:"Disinfecting Services"},
-   { id:3, href:"/services/janitorial-services", title:"Janitorial Services"},
-   { id:4, href:"/services/carpet-cleaning", title:"Carpet Cleaning"},
-   {id:5, href:"/services/washroom-cleaning-and-consumables", title:"Washroom & Consumables" }
+  { id: 0, href: "/services/standard-cleaning", title: "Standard Cleaning" },
+  { id: 1, href: "/services/deep-cleaning", title: "Deep Cleaning" },
+  { id: 2, href: "/services/disinfecting-services", title: "Disinfecting Services" },
+  { id: 3, href: "/services/janitorial-services", title: "Janitorial Services" },
+  { id: 4, href: "/services/carpet-cleaning", title: "Carpet Cleaning" },
+  { id: 5, href: "/services/washroom-cleaning-and-consumables", title: "Washroom & Consumables" }
 ];
 
 export const facilities = [
-    { href: "/facilities/office-buildings-cleaning", title: "Office Buildings" },
-    { href: "/facilities/retail-stores-cleaning", title: "Retail & Shopping centers" },
-    { href: "/facilities/day-care-cleaning", title: "DayCare & Preschool" },
-    { href: "/facilities/school-cleaning", title: "Schools & Universities" },
-    { href: "/facilities/medical-offices-cleaning", title: "Medical Offices & Clinics" },
-    { href: "/facilities/hotel-cleaning", title: "Hotels & Resorts" },
-    { href: "/facilities/recreational-facilities-cleaning", title: "Sports & Recreational Centers" },
-    { href: "/facilities/post-construction-cleaning", title: "Post Construction Clean" },
-  ];
+  { href: "/facilities/office-buildings-cleaning", title: "Office Buildings" },
+  { href: "/facilities/retail-stores-cleaning", title: "Retail & Shopping centers" },
+  { href: "/facilities/day-care-cleaning", title: "DayCare & Preschool" },
+  { href: "/facilities/school-cleaning", title: "Schools & Universities" },
+  { href: "/facilities/medical-offices-cleaning", title: "Medical Offices & Clinics" },
+  { href: "/facilities/hotel-cleaning", title: "Hotels & Resorts" },
+  { href: "/facilities/recreational-facilities-cleaning", title: "Sports & Recreational Centers" },
+  { href: "/facilities/post-construction-cleaning", title: "Post Construction Clean" },
+];
 
-  export const footerCompanyLinks = [
-    { href: "/", title: "Home" },
-    { href: "/blog", title: "Blog" },
-    { href: "/about", title: "About" },
-    { href: "/contact", title: "Contact" },
-    { href: "/quote", title: "Get a quote" },
-  ];
+export const footerCompanyLinks = [
+  { href: "/", title: "Home" },
+  { href: "/blog", title: "Blog" },
+  { href: "/about", title: "About" },
+  { href: "/contact", title: "Contact" },
+  { href: "/quote", title: "Get a quote" },
+];
+
+export const indexFacilitiesList = [
+  {
+    title: "Office buildings",
+    href: "facilities/office-buildings-cleaning",
+    imgUrl: "/office-building.svg",
+  },
+  {
+    title: "Retail stores and shopping centers",
+    href: "facilities/retail-stores-cleaning",
+    imgUrl: "/supermarket.svg",
+  },
+  {
+    title: "Day Care and Preschool",
+    href: "facilities/day-care-cleaning",
+    imgUrl: "/day-care-center.svg",
+  },
+  {
+    title: "Schools and universities",
+    href: "facilities/school-cleaning",
+    imgUrl: "/university.svg",
+  },
+  {
+    title: "Medical Offices and clinics",
+    href: "facilities/medical-offices-cleaning",
+    imgUrl: "/hospital.svg",
+  },
+  {
+    title: "Hotels and resorts",
+    href: "facilities/hotel-cleaning",
+    imgUrl: "/hotel.svg",
+  },
+  {
+    title: "Sports and recreational facilities",
+    href: "facilities/recreational-facilities-cleaning",
+    imgUrl: "/gym.svg",
+  },
+  {
+    title: "Post Construction Clean up",
+    href: "facilities/post-construction-cleaning",
+    imgUrl: "/construction.svg",
+  },
+];
+
+export const testimonials = [
+  {
+    text: "It was a great pleasure engaging your services at our office and for the impressive services at your most inconvenient time frame. This depicts commitment, reliability and positive business drive.",
+    author: "Alex Emmanuel",
+    business: "VSF CONSULTING INC"
+  }
+
+]
