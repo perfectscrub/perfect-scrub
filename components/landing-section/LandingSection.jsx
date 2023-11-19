05a9b4e0ebef7fe345bc5219bfb346ccf5a50c78
abeonweb <bmospecific@yahoo.com>
2023-11-19 18:22:30 +0100

    added padding to text container

--- components/landing-section/LandingSection.jsx
+++ components/landing-section/LandingSection.jsx
@@ -4,7 +4,7 @@ import MonogramOverlay from "../monogram-overlay/MonogramOverlay"
 const LandingSection = ({bgImg, title, type}) => {
   return (
     <section className={`${bgImg} flex`}>
-          <div className="z-20 self-center md:self-start md:pl-16">
+          <div className="z-20 self-center md:self-start md:pl-16 px-2">
             <h2 className="text-xs lg:text-base uppercase font-bold mb-2 lg:mb-4 text-gray-100">
               {type}
             </h2>
