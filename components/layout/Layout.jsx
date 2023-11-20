d3f42588a7ff050674703b10c8557ad43bbe62cd
abeonweb <bmospecific@yahoo.com>
2023-11-20 15:56:31 +0100

    moved state to layout,  add close nav function to all components

--- components/layout/Layout.jsx
+++ components/layout/Layout.jsx
@@ -1,13 +1,21 @@
-import React from "react";
+import { useState } from "react";
 import Footer from "../footer/Footer";
 import Navbar from "../navbar/Navbar";
 
 const Layout = ({ children }) => {
+  const [toggleNav, setToggleNav] = useState(false);
+
+  const handleNavClose = () => {
+    if (toggleNav) setToggleNav(false);
+  };
+
   return (
     <main role="main" className="layout">
-      <Navbar />
-      <main className="layout-main">{children}</main>
-      <Footer />
+      <Navbar toggleNav={toggleNav} setToggleNav={setToggleNav} handleNavClose={handleNavClose} />
+      <main className="layout-main" onClick={handleNavClose}>
+        {children}
+      </main>
+      <Footer handleNavClose={handleNavClose}/>
     </main>
   );
 };
