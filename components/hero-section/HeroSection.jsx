70a9fb5811182fc90a8ab302bb58acac8ea0c9fc
abeonweb <bmospecific@yahoo.com>
2023-11-28 23:15:09 +0100

    added landing section update

--- components/hero-section/HeroSection.jsx
+++ components/hero-section/HeroSection.jsx
@@ -1,9 +1,10 @@
-import BackgroundSlider from "../background-slider/BackgroundSlider";
+import LandingSection from "../landing-section/LandingSection";
 
 const HeroSection = () => {
   return (
     <section className="hero-container">
-      <BackgroundSlider />
+      {/* custom hero that depicts commercial and residential cleaning */}
+      <LandingSection bgImg={"commercial-cleaning-image"} title={"A Proper Cleaner"} type={"Commercial Cleaning"}/>
     </section>
   );
 };
