2738322a6bf913cb2b39c4d33f57a982330ceae1
abeonweb <bmospecific@yahoo.com>
2023-12-12 14:38:38 +0100

    image dimensions adjusted changes to styling

--- components/highlight/Highlight.jsx
+++ components/highlight/Highlight.jsx
@@ -5,9 +5,9 @@ import HomeFacilitiesList from "../homeFacilitiesList/HomeFacilitiesList";
 
 const Highlight = () => {
   return (
-    <section className="flex flex-col">
-          <div className="flex flex-col lg:flex-row gap-8 p-4 mt-12 md:mt-0 lg:p-20">
-            <Image width={500} height={400} className="w-full lg:w-1/2 object-cover" src="/two-women-cleaning-office.jpg" alt="" />
+    <section className="flex flex-col pt-10">
+          <div className="flex flex-col-reverse lg:flex-row gap-8 p-4 mt-12 md:mt-0 lg:p-20">
+            <Image width={500} height={400} className="w-full md:w-1/2 object-cover mx-auto md:mb-10" src="/two-women-cleaning-office.jpg" alt="" />
             <article className="w-full lg:w-1/2 text-left">
               <h2 className="text-3xl font-sans font-light ">
                 <Link href="/commercial-cleaning/office-buildings-cleaning" className="hover:underline">
@@ -26,14 +26,14 @@ const Highlight = () => {
                 Because we believe that our dedication contributes to the overall success of the companies we serve.</p>
             </article>
           </div>
-          <div className="px-4 mb-6 md:mx-8 lg:mb-14 flex flex-col xl:flex-row gap-4">
-            <article className="text-left pl-2 max-w-4xl flex flex-col">
+          <div className="px-4 mb-6 md:mx-8 lg:mb-14 flex flex-col lg:flex-row lg:gap-x-4">
+            <article className="w-full lg:w-1/2 text-left pl-2 max-w-2xl flex flex-col">
               <h2 className="text-3xl font-sans font-light"><span className="font-bold">Facilities</span> we serve</h2>
               <p className="my-5">We provide our services to a wide range of facilities and establishments. Some of the
                 facilities we serve include:</p>
               <HomeFacilitiesList />
             </article>
-            <Image width={300} height={200} src="/facilities/gym-cleaner.jpg" alt="a modern gym" className="w-full md:w-1/2 lg:pr-10 lg:mt-10" />
+            <Image width={600} height={400} src="/facilities/gym-cleaner.jpg" alt="a modern gym" className="w-full md:w-1/2 lg:mt-10" />
           </div>
         </section>
   )
