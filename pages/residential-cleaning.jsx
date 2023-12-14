b4f62ea5af2592e2067b98faf20b890b1fdaa4c9
abeonweb <bmospecific@yahoo.com>
2023-12-14 14:14:21 +0100

    corrected title tag content

---
+++ pages/residential-cleaning.jsx
@@ -0,0 +1,31 @@
+import Head from "next/head";
+import EasySteps from "@/components/easy-steps/EasySteps";
+import ServicesSection from "@/components/services-section/ServicesSection";
+import HeroLandingSection from "@/components/hero-landing/HeroLandingSection";
+
+const Residential = () => {
+  return (
+    <>
+      <Head>
+        <meta
+          content="We offer top notch residential cleaning. Booking a clean with us is quick and easy"
+          name="description"
+        />
+        <title>Perfect Scrub Cleaning Services- Residential Cleaning Page</title>
+      </Head>
+      <HeroLandingSection
+        bgImg={"residential-cleaning-image"}
+        title={"We Leave your home spotless"}
+        type={"Residential Cleaning"}
+      />
+      {/* Booking */}
+      
+      {/* explain the service */}
+      <ServicesSection />
+      {/* contact */}
+      <EasySteps imageOption={"man"} />
+    </>
+  );
+};
+
+export default Residential;
