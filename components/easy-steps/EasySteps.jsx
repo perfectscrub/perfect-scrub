0ce0f4fcc766bba4f32d09e8e0b2ea10113b5bb1
abeonweb <bmospecific@yahoo.com>
2023-12-20 08:32:38 +0100

    update steps component to render one of two button conditionally separated guarantee section to separate component output cards from an array

--- components/easy-steps/EasySteps.jsx
+++ components/easy-steps/EasySteps.jsx
@@ -1,85 +1,41 @@
-import Image from 'next/image';
+import Image from "next/image";
 import Link from "next/link";
 
-const imageOptions = {
-    man: {src:"/man-holding-brush.png", alt:"man ready to clean"},
-    woman: {src:"/woman-with-spray-bottle.png", alt:"woman with a spray bottle"},
-    worker: {src:"/woman-with-cap.png", alt:"Cleaning lady holding a window wiper"}
-}
-
-const EasySteps = ({imageOption}) => {
+const EasySteps = ({ type, steps }) => {
   return (
-    <section className="flex flex-col px-4 py-16 justify-center bg-white">
-          <div className="flex flex-col-reverse md:flex-row justify-between">
-            <h2 className="font-semibold text-2xl lg:text-4xl text-gray-700 px-1 py-5 mb-8 border-t-4 rounded-xl md:ml-8 self-center  border-b-8 border-green-500">
-              Getting your <br />
-              Perfect Scrub Clean <br /> is as EASY AS:
-              {/* <span className="text-blue-600">1-2-3</span> */}
-            </h2>
-            <div className="lg:block mb-0 lg:-mb-16 w-1/2 self-center">
-              <Image
-                width={300}
-                height={375}
-                className="object-cover"
-                src={imageOptions[imageOption].src}
-                alt= {imageOptions[imageOption].alt}
-              />
+    <section className="flex flex-col px-4 py-20 justify-center bg-white text-center">
+      <h2 className="text-2xl md:text-3xl lg:text-4xl font-bold">
+        Get it done in 3 Easy Steps!
+      </h2>
+      <div className="flex flex-col md:flex-row flex-wrap px-4 py-16 justify-center bg-white">
+        {steps.map((step) => (
+          <article key={step.title} className="max-w-xs py-4 px-3 mx-4">
+            <Image
+              className="rounded-[125px] mx-auto"
+              width={250}
+              height={250}
+              src={step.src}
+              alt={step.alt}
+            />
+            <div className="px-3 py-4">
+              <h3 className="text-lg md:text-xl lg:text-2xl pb-3 leading-relaxed font-semibold">
+                {step.title}
+              </h3>
+              <p className="">{step.text}</p>
             </div>
-          </div>
-          <div className="flex flex-col md:flex-row p-6 lg:px-4 justify-center text-center bg-white">
-            <div className="w-full md:w-72 border-8 rounded-2xl border-blue-600 px-2 py-6 mb-8 lg:ml-10 font-plexSerif font-semibold flex flex-col lg:mb-16 pb-2 ring-4 ring-blue-300 ring-inset">
-              <span className="text-2xl lg:text-4xl lg:mr-2 mb-4 text-green-500">
-                #1
-              </span>
-              <span className="text-xl lg:text-2xl px-2 pb-8 lg:mr-2 text-gray-700">
-                Use our
-                <Link className="text-blue-600 hover:underline" href="/quote">
-                  {" "}
-                  get a quote
-                </Link>
-                {" "}form to inform us about your cleaning needs
-              </span>
-            </div>
-            <div className="w-full md:w-72 border-8 rounded-2xl border-blue-600 px-2 py-6 mb-8 lg:ml-10 font-plexSerif font-semibold flex flex-col lg:mb-16 pb-2 ring-4 ring-blue-300 ring-inset">
-              <span className="text-2xl lg:text-4xl lg:mr-2 mb-4 text-green-500">
-                #2
-              </span>
-              <span className="text-xl lg:text-2xl px-2 pb-8 lg:mr-2 text-gray-700">
-                We will get in touch with you and process your cleaning request
-              </span>
-            </div>
-            <div className="w-full md:w-72 border-8 rounded-2xl border-blue-600 px-2 py-6 mb-8 lg:ml-10 font-plexSerif font-semibold flex flex-col lg:mb-16 pb-2 ring-4 ring-blue-300 ring-inset">
-              <span className="text-2xl lg:text-4xl lg:mr-2 mb-4 text-green-500">
-                #3
-              </span>
-              <span className="text-xl lg:text-2xl px-2 pb-8 lg:mr-2 text-gray-700">
-                A cleaning crew will be at your location on your designated
-                start date
-              </span>
-            </div>
-          </div>
-          <div className="py-6 mb-8 font-sans font-bold grid grid-col-1 lg:mb-20 pb-10">
-            {/* <h2 className="font-sans text-4xl">A job well done is what we stand for</h2> */}
-            <h2 className="font-sans text-4xl text-center mb-6">
-              A job well done. That is our promise to you.
-            </h2>
-            <p className="font-plexSerif text-2xl lg:text-3xl lg:mr-2 mb-10 text-green-500 text-center">
-              100% SATISFACTION GUARANTEE
-            </p>
-            <p className="text-lg lg:text-2xl mb-20 m-auto text-gray-700 text-center">
-              If you are not totally satisfied with our performance - we will
-              clean it again, free.
-            </p>
-            <Link
-              className="text-2xl py-4 px-8 m-auto text-white bg-green-600 hover:shadow-xl text-center no-underline 
-              uppercase border-2 border-green-600 hover:bg-green-500 hover:border-green-500 scale-100 hover:scale-105 self-center cursor-pointer"
-              href="/contact"
-            >
-              contact us
-            </Link>
-          </div>
-        </section>
-  )
-}
+          </article>
+        ))}
+      </div>
+      <div className="pb-10">
+        <Link
+          href={type === "residential" ? "/booking" : "/quote"}
+          className={`rounded py-6 px-10 text-white ${type==="residential"? "bg-red-500":"bg-blue-500"} text-3xl hover:shadow-2xl`}
+        >
+          {type === "residential" ? "Book Now" : "Get Quote"}
+        </Link>
+      </div>
+    </section>
+  );
+};
 
 export default EasySteps;
