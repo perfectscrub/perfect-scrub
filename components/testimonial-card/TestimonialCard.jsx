302daf9eafb764cc71eb92c9e241deb4833b2833
abeonweb <bmospecific@yahoo.com>
2024-02-02 13:17:08 +0100

    added code for slider

--- components/testimonial-card/TestimonialCard.jsx
+++ components/testimonial-card/TestimonialCard.jsx
@@ -1,19 +1,43 @@
+import { useState } from "react";
+import Image from "next/image";
+const TestimonialCard = ({ testimonial: { text, author, business } }) => {
+  const [toggleText, setToggleText] = useState(false);
 
-const TestimonialCard = ({testimonial:{ text, author, business }}) => {
+  const handleToggleText = () => {};
+
   return (
-    <div className="max-w-xl mx-10 bg-transparent mb-16 lg:mb-0">
-      <p className="pt-8 pb-2 text-center text-gray-600">
-        <span className="font-bold text-5xl">“</span>
-        {text}
-        {/* <span className="font-bold">”</span> */}
-      </p>
-      <div className="flex flex-col items-center pb-6 bg-transparent">
-        {/* <Image width={40} height={40} className="rounded-full w-24 border-4 -mt-12 border-white shadow-inner"
+    <div className="slide">
+      <div className="slideContainer">
+        <Image width={100} height={40} src={"/five-stars.png"} className="mx-auto"/>
+        {text.length <= 250 ? (
+          <p className="w-full block py-2 text-center text-gray-600">
+            <span className="font-bold text-5xl">“</span>
+            {/* <span className="font-bold">”</span> */}
+            {text}
+          </p>
+        ) : (
+          <p className="block py-2 text-center text-gray-600">
+            <span className="font-bold text-5xl">“</span>
+            {/* <span className="font-bold">”</span> */}
+            {`${text.substring(0, 250)}... `}
+            {/* <span
+              className="underline text-gray-800 hover:text-green-500 whitespace-nowrap"
+              onClick={handleToggleText}
+            >
+              {toggleText ? "show less" : "read more"}
+            </span> */}
+          </p>
+        )}
+        <div className="flex flex-col items-center pb-6 bg-transparent">
+          {/* <Image width={40} height={40} className="rounded-full w-24 border-4 -mt-12 border-white shadow-inner"
             src="/Testimonials/smiling_man.png" alt=""/> */}
-        <span className="text-sm font-bold mt-2 tracking-wider text-gray-600">
-          {author}
-        </span>
-        <span className="text-sm text-gray-500">{business}</span>
+          <span className="text-sm font-bold mt-2 tracking-wider text-blue-800">
+            - {author}
+          </span>
+          {business === "N/A" ? null : (
+            <span className="text-sm text-gray-500">{business}</span>
+          )}
+        </div>
       </div>
     </div>
   );
--- components/testimonials/Testimonials.jsx
+++ components/testimonials/Testimonials.jsx
@@ -1,19 +1,56 @@
-import React from 'react'
+import { useEffect, useState, useRef } from "react";
 import TestimonialCard from "../testimonial-card/TestimonialCard";
 import { testimonials } from "@/data";
+import Image from "next/image";
+
+const delay = 6000;
 
 const Testimonials = () => {
-  return (
-    <section className="flex flex-col items-center pt-20 lg:pt-40 pb-12 bg-green-200">
-          <h2 className="tracking-wider text-4xl lg:text-5xl text-gray-700">Testimonials</h2>
-          <div className="flex flex-row flex-wrap justify-center pt-10 pb-8 max-w-screen-2xl">
-            {/* End of Testimonial Card*/}
-            {
-                testimonials.map(testimonial => <TestimonialCard key={testimonial.author} testimonial={testimonial} />)
-            }
-          </div>
-        </section>
-  )
-}
+  const [sliderPosition, setSliderPosition] = useState(0);
+  const sliderTimerRef = useRef(null);
 
-export default Testimonials
+  const resetTimeout = () => {
+    if (sliderTimerRef.current) clearTimeout(sliderTimerRef.current);
+  };
+
+  useEffect(() => {
+    sliderTimerRef.current = setTimeout(() => {
+      setSliderPosition((prevPosition) =>
+        prevPosition === testimonials.length - 1 ? 0 : prevPosition + 1
+      );
+    }, delay);
+
+    return () => {
+      resetTimeout();
+    };
+  }, [sliderPosition]);
+  return (
+    <section className="flex flex-col items-center pt-10 lg:pt-40 pb-12 bg-green-200">
+      <h2 className="tracking-wider text-3xl lg:text-5xl text-gray-700 text-center">
+        Our Client Testimonials
+      </h2>
+      <div className="flex flex-col-reverse md:flex-row justify-around items-center">
+        <Image width={500} height={400} src={"/testimonials.png"}className="md:self-end p-5" />
+        <div className="slideShow">
+          <div
+            className="slider"
+            style={{
+              transform: `translate3d(${-sliderPosition * 100}%, 0, 0)`,
+            }}
+          >
+            {testimonials.map((testimonial) => (
+              <TestimonialCard
+                key={testimonial.author}
+                testimonial={testimonial}
+                className="slide"
+              />
+            ))}
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Testimonials;
+// flex flex-row flex-wrap justify-center pt-10 pb-8 max-w-screen-2xl
