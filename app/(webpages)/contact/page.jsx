53b651cb8ee4d73c9d34ec264bdcc440204fbd55
abeonweb <bmospecific@yahoo.com>
2025-01-22 22:53:48 +0100

    update phone number

--- app/(webpages)/contact/page.jsx
+++ app/(webpages)/contact/page.jsx
@@ -62,7 +62,7 @@ const Contact = () => {
                       Telephone Number
                     </span>
                     <span className="block font-medium text-gray-600">
-                      <Link href="tel:6474736478">437 524 5122</Link>
+                      <Link href="tel:4375245122">437 524 5122</Link>
                     </span>
                   </div>
                 </div>
